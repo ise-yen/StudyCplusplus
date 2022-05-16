b0c36b0718e0b05c8ccc23b34e7b6b74eccaa8fa
Ise이제 <ydkrkek@gmail.com>
2022-05-17 03:32:53 +0900

    [Feat] Complete Week1-1
    공통 문자열 정답 완료

---
+++ StudyCPP/InflearnCondingTest/Week1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	int N;
+	string word; //  N개의 단어들
+	vector<string> words;
+	string answer; // 정답단어
+
+	// N개의 단어들 입력받기
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> word;
+		words.push_back(word);
+	}
+
+	// 제일 짧은 길이의 단어 찾기
+	string shortWord = words[0];
+
+	for (int i = 0; i < N; i++) {
+		if (shortWord.length() > words[i].length()) {
+			shortWord = words[i];
+		}
+	}
+
+	// 글자가 같은 단어 판별
+	int cnt = 1;
+	for (int charI = 0; charI < shortWord.length(); charI++) {
+		for (int j = 0; j < N-1; j++) {
+			if (words[j][charI] == words[j + 1][charI]) {
+				cnt++;
+			}
+		}
+
+		if (cnt == N) {
+			answer.push_back(words[0][charI]);
+			cnt = 1;
+		}
+		else break;
+	}
+
+	cout << answer;
+	return 0;
+}
